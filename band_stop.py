b40145a177fb9d3443231932d8ced32bf881d832
Juha Vierinen <jvierine@gmail.com>
2022-06-28 11:18:54 +0000

    small modificatiosn

--- band_stop.py
+++ band_stop.py
@@ -4,7 +4,7 @@ import scipy.io.wavfile as sw
 import scipy.signal as ss
 
 # read audio file (wav format)
-ts = sw.read("test.wav")
+ts = sw.read("elysium.wav")
 sr = ts[0]        # sample rate
 clip = ts[1]      # extract audio file as numpy data vector
 
@@ -12,11 +12,17 @@ if len(clip.shape)==2: # if stereo, only use one channel
     print("using only one stereo channel. read on")
     clip = ts[1][:,0]
 
-# length of filter
-N = int(len(clip)/20)
+print(len(clip))
 
-f0 = 150
-f1 = 400
+# length of filter
+#N = int(len(clip)/20)
+
+f0 = 80
+f1 = 1e3
+
+# time-frequency uncertainty
+N = int(20.0*sr*(1.0/(f1-f0)))
+print(N)
 
 om0 = 2*n.pi*f0
 om1 = 2*n.pi*f1
@@ -35,13 +41,19 @@ for i in range(N):
     else:
         h[i] = (n.sin(om0_hat*(i - int(N/2))) / ((i - int(N/2))*n.pi) - n.sin(om1_hat*(i - int(N/2))) / ((i - int(N/2))*n.pi))*w[i]
 
+plt.plot(h)
+plt.show()
+
 print("computing convolution")
-res = n.convolve(h,clip[0:N],mode="full")
+res = n.convolve(h,clip,mode="full")
 
 # normalize to unity
 res = res/(n.max(n.abs(res)))
 
 plt.plot(res)
+plt.show()
+
+plt.plot(n.fft.fftshift(n.fft.fftfreq(len(res),d=1.0/sr))/1e3,10.0*n.log10(n.abs(n.fft.fftshift(n.fft.fft(res)))**2.0))
 plt.show()
 
 print("Saving filtered.wav")
