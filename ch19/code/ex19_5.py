2bee5039c1422e495a71ba1d8ef6e32b71f0dbc5
jje041 <jje041@ifilab113-lnx.stud.cs.uit.no>
2022-06-30 14:34:46 +0200

    code for plotting diagram

---
+++ ch19/code/ex19_5.py
@@ -0,0 +1,48 @@
+import numpy as n
+import matplotlib.pyplot as plt
+import sys
+
+# partition the range (-pi,pi)
+omhat = n.linspace(-n.pi,n.pi,num=100000)
+
+# roots/poles of diagram a) and b)
+alpha1 = (2*n.sqrt(2))**(-1) + (2*n.sqrt(2))**(-1)*1j
+alpha2 = (2*n.sqrt(2))**(-1) - (2*n.sqrt(2))**(-1)*1j
+
+# roots of diagram c)
+beta1 = -(2*n.sqrt(2))**(-1) + (2*n.sqrt(2))**(-1)*1j
+beta2 = -(2*n.sqrt(2))**(-1) - (2*n.sqrt(2))**(-1)*1j
+
+# system functions
+def Ha(z):
+    return (z - 1)*(z - alpha1*z)*(z - alpha2)/(z**3)
+
+def Hb(z):
+    return (z)/(z - 1)*(z - alpha1)*(z - alpha2)
+
+def Hc(z):
+    return (z + 1)*(z - beta1)*(z - beta2)/(z**3)
+
+# function to convert the system function to the magnitude response function
+def magn_resp(func):
+    function = n.abs(func(n.exp(1j*omhat)))
+    return function
+
+fig, axs = plt.subplots(3)
+fig.suptitle("Magnitude response")
+axs[0].plot(omhat,magn_resp(Ha),label="a)",color="blue")
+axs[1].plot(omhat,magn_resp(Hb),label="b)",color="red")
+axs[1].set_ylim(0,10)   # limit diagram b) as it is unstable
+axs[2].plot(omhat,magn_resp(Hc),label="c)",color="orange")
+fig.legend()
+
+# set the labels
+for i in range(len(axs)):
+    axs[i].set_xlabel("$\hat{\omega}$")
+    axs[i].set_ylabel("$|\mathcal{H}(\hat{\omega})|$")
+
+plt.tight_layout()
+plt.savefig("../figures/magn_resp_diag")
+
+if len(sys.argv) == 1:
+    plt.show()
