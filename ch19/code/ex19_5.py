707abaae1aaa36485edfe684d82d0b810a415b6a
jrn <jje041@post.uit.no>
2023-11-29 14:41:24 +0100

    Formatted both LaTex and Python code, so it is easier to read. Fix some errors and rewrote a couple of lines.

--- ch19/code/ex19_5.py
+++ ch19/code/ex19_5.py
@@ -1,47 +1,52 @@
-import numpy as n
+from typing import Callable
+
 import matplotlib.pyplot as plt
+import numpy as np
 
-# partition the range (-pi,pi)
-omhat = n.linspace(-n.pi,n.pi,num=100000)
+# Partition the range (-pi, pi).
+omhat = np.linspace(-np.pi, np.pi, num=100000)
 
-# roots/poles of diagram a) and b)
-alpha1 = (2*n.sqrt(2))**(-1) + (2*n.sqrt(2))**(-1)*1j
-alpha2 = (2*n.sqrt(2))**(-1) - (2*n.sqrt(2))**(-1)*1j
+# Roots/poles of diagram a) and b).
+alpha1 = (2*np.sqrt(2))**(-1) + (2*np.sqrt(2))**(-1)*1j
+alpha2 = (2*np.sqrt(2))**(-1) - (2*np.sqrt(2))**(-1)*1j
 
-# roots of diagram c)
-beta1 = -(2*n.sqrt(2))**(-1) + (2*n.sqrt(2))**(-1)*1j
-beta2 = -(2*n.sqrt(2))**(-1) - (2*n.sqrt(2))**(-1)*1j
+# Roots of diagram c).
+beta1 = -(2*np.sqrt(2))**(-1) + (2*np.sqrt(2))**(-1)*1j
+beta2 = -(2*np.sqrt(2))**(-1) - (2*np.sqrt(2))**(-1)*1j
 
-# system functions
-def Ha(z):
+
+def Ha(z: np.ndarray) -> np.ndarray:
     return (z - 1)*(z - alpha1*z)*(z - alpha2)/(z**3)
 
-def Hb(z):
+
+def Hb(z: np.ndarray) -> np.ndarray:
     return (z)/(z - 1)*(z - alpha1)*(z - alpha2)
 
-def Hc(z):
+
+def Hc(z: np.ndarray) -> np.ndarray:
     return (z + 1)*(z - beta1)*(z - beta2)/(z**3)
 
-# function to convert the system function to the magnitude response function
-def magn_resp(func):
-    function = n.abs(func(n.exp(1j*omhat)))
-    return function
+
+def magn_resp(func: Callable) -> np.ndarray:
+    """Function to convert the system function to the magnitude response function."""
+    return np.abs(func(np.exp(1j*omhat)))
+
 
 fig, axs = plt.subplots(3)
 fig.suptitle("Magnitude response")
-axs[0].plot(omhat,magn_resp(Ha),label="a)",color="blue")
-axs[1].plot(omhat,magn_resp(Hb),label="b)",color="red")
-axs[1].set_ylim(0,10)   # limit diagram b) as it is unstable
-axs[2].plot(omhat,magn_resp(Hc),label="c)",color="orange")
+axs[0].plot(omhat, magn_resp(Ha), label="a)", color="blue")
+axs[1].plot(omhat, magn_resp(Hb), label="b)", color="red")
+axs[1].set_ylim(0, 10)   # Limit diagram b) as it is unstable.
+axs[2].plot(omhat, magn_resp(Hc), label="c)", color="orange")
 fig.legend()
 
-# set the labels
-for i in range(len(axs)):
-    axs[i].set_xlabel("$\hat{\omega}$")
-    axs[i].set_ylabel("$|\mathcal{H}(\hat{\omega})|$")
+# Set the labels.
+for ax in axs:
+    ax.set_xlabel(r"$\hat{\omega}$")
+    ax.set_ylabel(r"$|\mathcal{H}(\hat{\omega})|$")
 
 plt.tight_layout()
-# call this if needed
+# Call this if needed.
 # plt.show()
 
 try:
