953ae28b67c7c4e92e5ee1f1d2c11c01296e5f86
jrn <jje041@post.uit.no>
2024-06-23 13:37:54 +0200

    Cleaned up the complex algebra chapter.

--- ch03/code/ex3.6.py
+++ ch03/code/ex3.6.py
@@ -1,35 +1,35 @@
-import numpy as n
+import numpy as np
 import matplotlib.pyplot as plt
 
-# simple function to compute the kth order root of unity of degree m
-def rootUnity(k,m):
-    return n.exp((2*n.pi*k*1j)/m)
+# Simple function to compute the kth order root of unity of degree m.
+def root_unity(k: int, m: int) -> float:
+    return np.exp(2*np.pi*k*1j/m)
 
-# put all the roots in an array
-roots = n.array([rootUnity(0,5),rootUnity(1,5),rootUnity(2,5),rootUnity(3,5),rootUnity(4,5),rootUnity(0,5)])
 
-# partition an interval of one period
-t = n.linspace(0,2*n.pi,num=50)
+# Loop from 0 to 5, recall that the % operator computes the remainder, giving 0-4, then 0 again.
+roots = np.array([root_unity(k % 5, 5) for k in range(6)])
 
-# used to plot the unit circle
-x = n.cos(t)
-y = n.sin(t)
+# Partition an interval of one period.
+t = np.linspace(0, 2*np.pi, num=50)
 
-# extract the real and imaginary components
+# Used to plot the unit circle.
+x = np.cos(t)
+y = np.sin(t)
+
+# Extract the real and imaginary components.
 z1 = roots.real
 z2 = roots.imag
 
-# plotting
-plt.figure(figsize=(4,4))
-ax=plt.gca()
-plt.plot(z1,z2)
-plt.plot(x,y)
+plt.figure(figsize=(4, 4))
+ax = plt.gca()
+plt.plot(z1, z2)
+plt.plot(x, y)
 plt.grid(True)
 plt.xlabel("x")
 plt.ylabel("y")
 plt.title("Fifth roots of unity")
 ax.set_aspect("equal")
-# call this if needed
+# Call this if needed.
 # plt.show()
 try:
     plt.savefig("../figures/roots.png")
