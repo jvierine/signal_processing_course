13ecab0b42bef64d2d96ca5c28bd1eae1060bb1c
jrn <jje041@post.uit.no>
2024-06-24 14:19:09 +0200

    Updated ch07.

--- ch07/code/ex7_3a.py
+++ ch07/code/ex7_3a.py
@@ -1,31 +1,32 @@
-import numpy as n
+import numpy as np
 import matplotlib.pyplot as plt
 
-T = 1                   # period
-sample_rate = 1000.0    # sample rate
-N = 101                 # number of terms 
+T = 1                   # Period.
+sample_rate = 1000.0    # Sample rate.
+N = 101                 # Number of terms.
 
-# partition the t-axis
-t = n.arange(int(sample_rate*T))/sample_rate
+# Partition the t-axis.
+t = np.arange(int(sample_rate*T))/sample_rate
 
-# allocate an array for the signal
-xn = n.zeros(len(t),dtype=n.complex64)
+# Allocate an array for the signal.
+xn = np.zeros(len(t), dtype=np.complex64)
 
-# function for the Fourier coefficients
-def ck(k):
+
+def ck(k: int) -> float:
+    """Function for the Fourier coefficients."""
     if k == 0:
         return 1/10
     else:
-        return 1/(n.pi*k)*n.exp(-1j*n.pi*k/10)*n.sin(n.pi*k/10)
+        return 1/(np.pi*k)*np.exp(-1j*np.pi*k/10)*np.sin(np.pi*k/10)
 
-# loop through the indices and add the signal terms
-for i in range(-N,N+1):
-    xn += ck(i)*n.exp(1j*2*n.pi*i*t/T)
 
-# plot the result
-plt.plot(t,xn.real,color="blue")
-plt.plot(t,xn.imag,color="red")
+# Loop through the indices and add the signal terms.
+for i in range(-N, N+1):
+    xn += ck(i)*np.exp(1j*2*np.pi*i*t/T)
+
+plt.plot(t, xn.real, color="blue")
+plt.plot(t, xn.imag, color="red")
 plt.xlabel("Time (t)")
 plt.ylabel("$x_{N}(t)$")
-# if needed
+# If needed.
 # plt.plot()
--- code/009_square_wave/square_wave.py
+++ code/009_square_wave/square_wave.py
@@ -1,45 +1,44 @@
 #!/usr/bin/env python
 
-import numpy as n
+import numpy as np
 import matplotlib.pyplot as plt
 
+P = 0.1
+T = 1.0
 
-P=0.1
-T=1.0
+sample_rate = 1000.0
+t = np.arange(int(sample_rate*T))/sample_rate
 
-sample_rate=1000.0
-t=n.arange(int(sample_rate*T))/sample_rate
+zn = np.zeros(len(t), dtype=np.complex64)
 
-zn = n.zeros(len(t),dtype=n.complex64)
+N = 101
 
-N=101
-
-ks=n.arange(-N,N+1)
-cks=n.zeros(2*N+1,dtype=n.complex64)
-ki=0
-for k in range(-N,N+1):
+ks = np.arange(-N, N+1)
+cks = np.zeros(2*N+1, dtype=np.complex64)
+ki = 0
+for k in range(-N, N+1):
     if k == 0:
         zn += P/T
-        cks[ki]=P/T
+        cks[ki] = P/T
     else:
-        k2=float(k)
-        ck=(1.0/(n.pi*k2))*(n.exp(-1j*(n.pi/T)*k2*P))*n.sin(n.pi*k2*P/T)
-        cks[ki]=ck
-        zn+= ck*n.exp(1j*(2.0*n.pi/T)*k2*t)
-    ki+=1
+        k2 = float(k)
+        ck = (1.0/(np.pi*k2))*(np.exp(-1j*(np.pi/T)*k2*P))*np.sin(np.pi*k2*P/T)
+        cks[ki] = ck
+        zn += ck*np.exp(1j*(2.0*np.pi/T)*k2*t)
+    ki += 1
 
-# Plot the absolute values of the Fourier series coefficients
-plt.plot(2.0*n.pi*ks/T,n.abs(cks))
+# Plot the absolute values of the Fourier series coefficients.
+plt.plot(2.0*np.pi*ks/T, np.abs(cks))
 plt.show()
 
-# create a square wave signal
-square_wave=n.zeros(len(t),dtype=n.float64)
-square_wave[(0<t)&(t<P)]=1.0
+# Create a square wave signal.
+square_wave = np.zeros(len(t), dtype=np.float64)
+square_wave[(0 < t) & (t < P)] = 1.0
 
-plt.figure(figsize=(0.8*6,0.8*4))
-plt.plot(t,square_wave,label="Square wave",color="black")
-plt.plot(t,zn.real,label="Fourier series approximation (Real)",color="blue")
-plt.plot(t,zn.imag,label="Fourier series approximation (Imag)",color="red")
+plt.figure(figsize=(0.8*6, 0.8*4))
+plt.plot(t, square_wave, label="Square wave", color="black")
+plt.plot(t, zn.real, label="Fourier series approximation (Real)", color="blue")
+plt.plot(t, zn.imag, label="Fourier series approximation (Imag)", color="red")
 plt.legend()
 plt.xlabel("Time (t)")
 plt.ylabel("s(t)")
