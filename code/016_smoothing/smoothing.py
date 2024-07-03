a6a6e2bcb4f2279b8aee5be1aed6b5864d51d6a2
jrn <jje041@post.uit.no>
2024-07-03 22:04:20 +0200

    Updated code, fix some warnings.

--- code/016_smoothing/smoothing.py
+++ code/016_smoothing/smoothing.py
@@ -1,29 +1,25 @@
-#!/usr/bin/env python
-
-import numpy as n
+import numpy as np
 import matplotlib.pyplot as plt
 
-N_avg=15
+N_avg = 15
 
-# signal length
-N=500
-t=n.arange(N)
+# Signal length.
+N = 500
+t = np.arange(N)
 
-# signal with a rectangular pulse in the middle
-# Create a random-walk signal
-signal=n.cumsum(n.random.randn(N+1))
+# Signal with a rectangular pulse in the middle.
+# Create a random-walk signal.
+signal = np.cumsum(np.random.randn(N+1))
 
-# filter signal using an averaging filter
-filtered_signal=n.convolve(n.repeat(1.0/N_avg,N_avg),signal,mode="same")
+# Filter signal using an averaging filter.
+filtered_signal = np.convolve(np.repeat(1.0/N_avg, N_avg), signal, mode="same")
 
-plt.figure(figsize=(8,4))
-plt.plot(signal,label="Signal")
-plt.plot(filtered_signal,label="Running average")
+plt.figure(figsize=(8, 4))
+plt.plot(signal, label="Signal")
+plt.plot(filtered_signal, label="Running average")
 plt.xlabel("Time index $n$")
 plt.ylabel("Signal amplitude")
 plt.legend()
 plt.tight_layout()
 plt.savefig("smoothing.png")
 plt.show()
-
-
