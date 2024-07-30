319a4d7d179d53aa07ea86c2d73375d4e2cde0bc
jrn <jje041@post.uit.no>
2024-07-30 13:52:31 +0200

    Updated to PEP8 and restructured.

--- ch16/code/ex16_1b.py
+++ ch16/code/ex16_1b.py
@@ -1,21 +1,22 @@
-import numpy as n
 import matplotlib.pyplot as plt
+import numpy as np
 import scipy.io.wavfile
 
 audio = scipy.io.wavfile.read("b.wav")
 sample_rate = audio[0]
-# read only one channel of the stereo signal
-signal = audio[1][:,0]
 
-# the sample rate is
-print(sample_rate)  # output is 44100 Hz
+# Read only one channel of the stereo signal.
+signal = audio[1][:, 0]
 
-# partition the interval such that the units become seconds
-t = n.arange(len(signal))/(sample_rate)
+# The sample rate is:
+print(sample_rate)  # output is 44100 Hz.
 
-plt.plot(t,signal)
+# Partition the interval such that the units become seconds.
+t = np.arange(len(signal))/(sample_rate)
+
+plt.plot(t, signal)
 plt.xlabel("Time (s)")
-# call this if needed
+# Call this if needed.
 # plt.show()
 try:
     plt.savefig("../figures/audio.png")
--- code/022_window_functions/windowed_dft.py
+++ code/022_window_functions/windowed_dft.py
@@ -1,43 +1,39 @@
-import numpy as n
 import matplotlib.pyplot as plt
-import scipy.signal as s
+import numpy as np
+from scipy.signal.windows import hann
+
 # Using windowed DFT for spectral analysis of
-#signals Demonstrate that a window function allows weak signals to be
-#found better, due to less spectral leakage than if a rectangular
-#window is used.  import numpy as n import matplotlib.pyplot as plt
-#import scipy.signal as s from matplotlib import rc rc('text',
-#usetex=True)
+# signals Demonstrate that a window function allows weak signals to be
+# found better, due to less spectral leakage than if a rectangular
+# window is used.
 
-N=4096
-# sample indices
-nn=n.arange(N)
+N = 4096
+n = np.arange(N)
 
-freq1=0.01*n.pi
-freq2=0.1*n.pi
+freq1 = 0.01*np.pi
+freq2 = 0.1*np.pi
 
-# strong low-frequency signal signal
-strong_signal=1e6*n.cos(freq1*nn)
+# Strong low-frequency signal signal.
+strong_signal = 1e6*np.cos(freq1*n)
 
-# weak signal at higher frequency
-weak_signal=n.cos(freq2*nn)
+# Weak signal at higher frequency.
+weak_signal = np.cos(freq2*n)
 
-# sum strong and weak signal together
-y=strong_signal+weak_signal
+y = strong_signal + weak_signal
 
-# plot signals
 plt.subplot(311)
-plt.plot(nn,strong_signal)
+plt.plot(n, strong_signal)
 plt.xlabel("Sample ($n$)")
 plt.ylabel("$x_1[n]$")
-plt.title("Strong signal $\hat{\omega}=%1.2f$"%(freq1))
+plt.title(r"Strong signal $\hat{\omega}=%1.2f$" % (freq1))
 plt.subplot(312)
-plt.plot(nn,weak_signal)
+plt.plot(n, weak_signal)
 plt.xlabel("Sample ($n$)")
 plt.ylabel("$x_2[n]$")
-plt.title("Weak signal $\hat{\omega}=%1.2f$"%(freq2))
+plt.title(r"Weak signal $\hat{\omega}=%1.2f$" % (freq2))
 plt.subplot(313)
-# weak signal is impossible to see, because it has a 1e6 smaller amplitude
-plt.plot(nn,y)
+# Weak signal is impossible to see, because it has a 1e6 smaller amplitude.
+plt.plot(n, y)
 plt.xlabel("Sample ($n$)")
 plt.ylabel("$y[n]=x_1[n]+x_2[n]$")
 plt.title("Total signal")
@@ -45,22 +41,22 @@ plt.tight_layout()
 plt.savefig("windowed_signals.png")
 plt.show()
 
-# analyze spectrum
-# zero padded DFT to be of length 2*N
-Y=n.fft.fft(y,2*N)
-# windowed zero-padded DFT
-w=s.hann(N)
-# zero padded DFT to be of length 2*N
-WY=n.fft.fft(w*y,2*N)
-# frequencies in radians per sample
-om=n.fft.fftfreq(len(Y),d=1)*2.0*n.pi
-# reorder frequencies so that we go from -pi to pi instead of 0 to 2\pi
-om=n.fft.fftshift(om)
-Y=n.fft.fftshift(Y)
-WY=n.fft.fftshift(WY)
+# Analyze spectrum
+# zero padded DFT to be of length 2*N.
+Y = np.fft.fft(y, 2*N)
+# Windowed zero-padded DFT.
+w = hann(N)
+# Zero padded DFT to be of length 2*N.
+WY = np.fft.fft(w*y, 2*N)
+# Frequencies in radians per sample.
+om = np.fft.fftfreq(len(Y), d=1)*2.0*np.pi
+# Reorder frequencies so that we go from -pi to pi instead of 0 to 2\pi.
+om = np.fft.fftshift(om)
+Y = np.fft.fftshift(Y)
+WY = np.fft.fftshift(WY)
 
-plt.plot(om,10.0*n.log10(n.abs(Y)**2.0),label="DFT")
-plt.plot(om,10.0*n.log10(n.abs(WY)**2.0),label="Windowed DFT")
+plt.plot(om, 10.0*np.log10(np.abs(Y)**2.0), label="DFT")
+plt.plot(om, 10.0*np.log10(np.abs(WY)**2.0), label="Windowed DFT")
 plt.ylabel("Spectral power (dB)")
 plt.xlabel("Frequency (rad/s)")
 plt.title("Power spectrum")
